370a81828c3dc6adb78c8a0fc52cf8bf02114599
Kate <reena@myrambler.ru>
2022-09-07 23:34:54 +0300

    fix filter

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -18,6 +18,7 @@ function Main() {
 
   const [selectCategory, setSelectCategory] = useState(null)
   const [directSelect, setDirectSelect] = useState('')
+  const [filterSelect, setFilterSelect] = useState(true)
 
   const { directions } = useSelector( s => s)
   const { filter } = useSelector( s => s) 
@@ -33,18 +34,18 @@ function Main() {
     <div className='app'>
       <div className='nav'>
         <p>Отдаете</p>
-        {category.map((el, i) => <button key={i} value={el.codes} onClick={(e) => setSelectCategory(e.target.value)}>{el.name}</button>)}
+        {category.map((el, i) => <button key={i} value={el.codes} onClick={(e) => {setSelectCategory(e.target.value); setFilterSelect(false)}}>{el.name}</button>)}
       </div>
 
       <div className='wrapper'>
         <p>Получаете</p>
-        <select onChange={(e) => setDirectSelect(e.target.value)}>
+        <select onChange={(e) => {setDirectSelect(e.target.value); setFilterSelect(true)}}>
           {directions.filter(el => selectCategory ? selectCategory.split(',').includes(el.code) : true)
           .map((el, i) => <option key={i} value={el.code}>{el.name}</option>)}
         </select>
         <p>⇵</p>
         <select>
-          {filter.filter(el => el.from.code === directSelect)
+          {filter.filter(el => el.from.code === directSelect && filterSelect === true)
           .map(el => el.to.map((item, i) => <option key={i}>{item.name}</option>))}
         </select>
       </div>
